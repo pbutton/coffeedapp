2c275a32947a14bdfd774f36635bc2fd472f211f
Peter Button <peterabutton@gmail.com>
2015-06-25 22:29:08 +0200

    Added db and files

---
+++ core/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Location',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('title', models.CharField(max_length=300)),
+                ('description', models.TextField(null=True, blank=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+            ],
+        ),
+    ]
